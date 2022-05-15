d1eff92f36505196d8aea8812e11d03b22f5e0a4
audace-kwizera <companyfreshbeauty@gmail.com>
2022-05-15 20:45:11 +0200

    fix route register

--- backend/routes/usersCtrl.js
+++ backend/routes/usersCtrl.js
@@ -17,7 +17,42 @@ module.exports = {
 			return res.status(400).json({ 'error': 'Veuillez remplir ce champs' })
 		}
 
-		// Vérifier si l'user n'existe pas dans la base de donnée
+		// Vérifier si l'user n'existe pas dans la base de données
+		models.User.findOne({
+			attributes: ['email'],
+			where: { email: email }
+		})
+		.then(function(userFound) {
+			if(!userFound) {
+          bcrypt.hash(req.body.password, 10, function(err, bcryptedPassword) {
+            var newUser = models.User.create({
+              email: email,
+              username: username,
+              password: bcryptedPassword,
+              bio: bio,
+              isAdmin: 0
+            })
+            //Si ok création de l'user
+            .then(function(newUser) {
+              return res.status(201).json({
+                'userId': newUser.id
+              })
+            })
+            // Sinon affichage erreur
+            .catch(function(err) {
+              return res.status(500).json({ 'error': `Oups une erreur s'est produite, veuillez réessayer` })
+            })
+          })
+			} else {
+				//Si pseudo existe Affichage erreur
+				return res.status(409).json({ 'error': 'Oups ce pseudo existe déjà' })
+			}
+		})
+		.catch(function(err) {
+			// Si erreur inconnu
+			return res.status(500).json({ 'error': 'Oups on arrive pas à vérifier si le pseudo est déjà pris.<br />Veuillez réessayer' })
+		});
+
 	},
 	login: function(req, res) {
 
